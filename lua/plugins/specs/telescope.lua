a044e03fc23fbc0a5dac1906b4ca73cb8b75735e
Baha Uddin Tanvir <b_tanvir@outlook.com>
2025-08-25 21:56:24 +0600

    colorscheme and telescope update

--- lua/plugins/specs/telescope.lua
+++ lua/plugins/specs/telescope.lua
@@ -51,5 +51,20 @@ return {
         winblend = 0,
       },
     },
+    {
+      "tokyonight.nvim",
+      opts = {
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg }
+          hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+          hl.TelescopePromptNormal = { bg = prompt }
+          hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+          hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
+          hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+          hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
+        end,
+      },
+    },
   },
 }
